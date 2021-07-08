08289d44d901bb90d3c76bf9a2418372ae0006d5
Por-045 <6210110045@psu.ac.th>
2021-07-08 11:31:31 +0700

    4.Components & Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import WordCard from './WordCard';
+const word = "Hello";
 function App() {
   return (
     <div>
-      Hello World
-    </div>
+      <WordCard value={word}/>
+    </div>    
   );
 }
 
